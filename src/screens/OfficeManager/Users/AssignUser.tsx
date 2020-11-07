6765573071e0fb161280fcc8ee8ebe119016d62e
Andrew Oplas <andrewmoplas@gmail.com>
2020-11-07 17:59:18 +0800

    Check log

--- src/screens/OfficeManager/Users/AssignUser.tsx
+++ src/screens/OfficeManager/Users/AssignUser.tsx
@@ -69,6 +69,8 @@ const AssignUser = ({ match }) => {
 					dayjs(ca.date, 'YYYY-MM-DD', true).isSame(item.date, 'date'),
 				);
 
+				console.log(assignment);
+
 				return {
 					date: item.display,
 					day: item.day,
