5f1df9c9ba191224a81e009212956b5ea24356c1
Andrew Oplas <andrewmoplas@gmail.com>
2020-11-28 09:57:23 +0800

    Update constants

--- src/global/constants.tsx
+++ src/global/constants.tsx
@@ -1,8 +1,8 @@
-export const MAX_PAGE_SIZE = 1000000;
+export const MAX_PAGE_SIZE = 500;
 
-export const MAX_RETRY = 3;
+export const MAX_RETRY = 1;
 
-export const RETRY_INTERVAL_MS = 1000;
+export const RETRY_INTERVAL_MS = 300;
 
 export const NOT_FOUND_INDEX = -1;
 
