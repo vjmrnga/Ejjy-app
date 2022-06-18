ea20af0804c536f0c0ab7bef39fd9e35043e9047
Andrew <andrewmoplas@gmail.com>
2022-06-18 20:13:59 +0800

    fix reports screen

--- src/screens/BranchManager/Reports/index.tsx
+++ src/screens/BranchManager/Reports/index.tsx
@@ -1,12 +1,9 @@
-import { Spin, Tabs } from 'antd';
-import { toString } from 'lodash';
+import { Spin } from 'antd';
+import { Content, RequestErrors } from 'components';
+import { Box } from 'components/elements';
+import { request } from 'global';
+import { useProductCategories } from 'hooks/useProductCategories';
 import React, { useEffect, useState } from 'react';
-import { Content, RequestErrors } from '../../../components';
-import { Box } from '../../../components/elements';
-import { request } from '../../../global/types';
-import { useBranches } from 'hooks';
-import { useProductCategories } from '../../../hooks/useProductCategories';
-import { useQueryParams } from 'hooks';
 import { convertIntoArray } from 'utils';
 import { ReportsBranch } from './components/ReportsBranch';
 
@@ -16,45 +13,19 @@ export const Reports = () => {
 
 	// CUSTOM HOOKS
 	const {
-		data: { branches },
-	} = useBranches();
-	const {
 		getProductCategories,
 		status: productCategoriesStatus,
 		errors: productCategoriesErrors,
 	} = useProductCategories();
 
-	// VARIABLES
-	const {
-		params: { branchId: currentBranchId },
-		setQueryParams,
-	} = useQueryParams();
-
 	// METHODS
 	useEffect(() => {
-		getProductCategories(
-			{ branchId: currentBranchId },
-			({ status, data: responseData }) => {
-				if (status === request.SUCCESS) {
-					setProductCategories(responseData);
-				}
-			},
-		);
-	}, []);
-
-	useEffect(() => {
-		if (branches && !currentBranchId) {
-			onTabClick(branches?.[0]?.id);
-		}
-	}, [branches, currentBranchId]);
-
-	const onTabClick = (branchId) => {
-		setQueryParams({
-			branchId,
-			page: 1,
-			pageSize: 10,
+		getProductCategories({}, ({ status, data: responseData }) => {
+			if (status === request.SUCCESS) {
+				setProductCategories(responseData);
+			}
 		});
-	};
+	}, []);
 
 	return (
 		<Content className="Reports" title="Reports">
