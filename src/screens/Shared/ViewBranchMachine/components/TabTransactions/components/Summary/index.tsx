73e2965de7f17275d6de2615f2fd98cdb68e90aa
Andrew Oplas <andrewmoplas@gmail.com>
2023-02-27 20:41:59 +0800

    update transaction summary field values

--- src/screens/Shared/ViewBranchMachine/components/TabTransactions/components/Summary/index.tsx
+++ src/screens/Shared/ViewBranchMachine/components/TabTransactions/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import { Col, Row, Spin, Statistic } from 'antd';
+import { Col, Row, Space, Spin, Statistic } from 'antd';
 import { RequestErrors } from 'components';
 import { refetchOptions, timeRangeTypes } from 'global';
 import { useQueryParams, useTransactionsSummary } from 'hooks';
@@ -37,66 +37,100 @@ export const Summary = ({ branchMachineId }: Props) => {
 			/>
 
 			<Spin spinning={isTransactionsSummaryLoading}>
-				<Row gutter={[16, 16]}>
-					<Col md={8} sm={8} xs={24}>
-						<Statistic
-							title="Vatable Sales"
-							value={formatInPeso(summary?.vatable_sales)}
-						/>
-					</Col>
-					<Col md={16} sm={16} xs={24}>
-						<Statistic
-							title="Voided Total"
-							value={formatInPeso(summary?.total_voided_sales)}
-						/>
-					</Col>
+				<Space className="w-100" direction="vertical" size={16}>
+					<Row gutter={[16, 16]}>
+						<Col md={8} sm={0} xs={0} />
+						<Col md={8} sm={0} xs={0} />
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Gross Sales From POS"
+								value={formatInPeso(summary?.total_gross_sales)}
+							/>
+						</Col>
+					</Row>
 
-					<Col md={8} sm={8} xs={24}>
-						<Statistic
-							title="Vat Amount"
-							value={formatInPeso(summary?.vat_amount)}
-						/>
-					</Col>
-					<Col md={16} sm={16} xs={24}>
-						<Statistic
-							title="Discount Total"
-							value={formatInPeso(summary?.total_discount)}
-						/>
-					</Col>
+					<Row gutter={[16, 16]}>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Vat-Exempt Sales"
+								value={formatInPeso(summary?.vat_exempt_sales)}
+							/>
+						</Col>
 
-					<Col md={8} sm={8} xs={24}>
-						<Statistic
-							title="Vat Exempt Sales"
-							value={formatInPeso(summary?.vat_exempt_sales)}
-						/>
-					</Col>
+						<Col md={8} sm={0} xs={0} />
 
-					<Col md={16} sm={16} xs={24}>
-						<Statistic
-							title="Vat Amount"
-							value={formatInPeso(summary?.vat_amount)}
-						/>
-					</Col>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="(Total Deductions)"
+								value={formatInPeso(summary?.total_deductions)}
+							/>
+						</Col>
+					</Row>
+					<Row gutter={[16, 16]}>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Vatable Sales"
+								value={formatInPeso(summary?.vatable_sales)}
+							/>
+						</Col>
 
-					<Col md={8} sm={8} xs={24}>
-						<Statistic
-							title="TOTAL GROSS SALES"
-							value={formatInPeso(summary?.total_gross_sales)}
-						/>
-					</Col>
-					<Col md={8} sm={8} xs={24}>
-						<Statistic
-							title="TOTAL DEDUCTIONS"
-							value={formatInPeso(summary?.total_deductions)}
-						/>
-					</Col>
-					<Col md={8} sm={8} xs={24}>
-						<Statistic
-							title="NET SALES"
-							value={formatInPeso(summary?.total_net_sales)}
-						/>
-					</Col>
-				</Row>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Voided Transactions"
+								value={formatInPeso(summary?.total_voided_sales)}
+							/>
+						</Col>
+
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Net Sales (VAT Inclusive)"
+								value={formatInPeso(summary?.total_net_sales_vat_inclusive)}
+							/>
+						</Col>
+					</Row>
+					<Row gutter={[16, 16]}>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Vat Amount"
+								value={formatInPeso(summary?.vat_amount)}
+							/>
+						</Col>
+
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Discounts"
+								value={formatInPeso(summary?.total_discount)}
+							/>
+						</Col>
+
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="(VAT Amount)"
+								value={formatInPeso(summary?.vat_amount)}
+							/>
+						</Col>
+					</Row>
+					<Row gutter={[16, 16]}>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Gross Sales From POS"
+								value={formatInPeso(summary?.total_gross_sales)}
+							/>
+						</Col>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Total Deductions"
+								value={formatInPeso(summary?.total_deductions)}
+							/>
+						</Col>
+						<Col md={8} sm={8} xs={24}>
+							<Statistic
+								title="Net Sales (VAT Exclusive)"
+								value={formatInPeso(summary?.total_net_sales_vat_exclusive)}
+							/>
+						</Col>
+					</Row>
+				</Space>
 			</Spin>
 		</div>
 	);
